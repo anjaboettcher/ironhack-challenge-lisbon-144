0f145eb300b74821ce5a7fafe60d5e865028ea72
anjaboettcher <anjaverenaboettcher@gmail.com>
2019-08-06 09:32:11 +0100

    first commit

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Hello, my name is Anja and you are on my solution for the Ironhack challenge!</p>
+    </div>
+  )
+}
